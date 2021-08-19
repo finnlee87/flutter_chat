566ca252e9364741e23382086248547b35398244
li-xuefeng <xuefeng.li@linecorp.com>
2021-08-19 13:59:09 +0800

    edit

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -2,10 +2,13 @@ import 'package:chat_flutter/Constants.dart';
 import 'package:chat_flutter/widgets/chat_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    print(MediaQuery.of(context).size.width);
+    print(MediaQuery.of(context).size.height);
     return CupertinoPageScaffold(
       backgroundColor: CupertinoColors.systemGrey5,
       navigationBar: CupertinoNavigationBar(
@@ -20,11 +23,21 @@ class ChatPage extends StatelessWidget {
         ),
         trailing: CupertinoButton(
           // color: CupertinoColors.white,
+          padding: EdgeInsets.zero,
           child: Icon(
-            Icons.more_horiz,
+            Icons.more_vert,
             color: CupertinoColors.white,
           ),
-          onPressed: () {},
+          onPressed: () {
+            Fluttertoast.showToast(
+                msg: "This is Center Short Toast",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.red,
+                textColor: Colors.white,
+                fontSize: 16.0);
+          },
         ),
       ),
       child: ChatWidget(context),
